08232a5171aa3954dc1a28f9ccfa300c14896770
Hendro Wibowo <hendrothemail@gmail.com>
2023-07-29 17:33:36 +0700

    Setup cross terminal screen

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
 use std::error::Error;
+use std::io;
+use crossterm::{ExecutableCommand, terminal};
+use crossterm::cursor::{Hide, Show};
+use crossterm::terminal::{EnterAlternateScreen, LeaveAlternateScreen};
 use rusty_audio::Audio;
 
 fn main() -> Result<(), Box<dyn Error>> {
@@ -9,6 +13,17 @@ fn main() -> Result<(), Box<dyn Error>> {
     audio.add("pew", "./sounds/pew.wav");
     audio.add("startup", "./sounds/startup.wav");
     audio.add("win", "./sounds/win.wav");
+    audio.play("startup");
+
+    let mut stdout = io::stdout();
+    terminal::enable_raw_mode()?;
+    stdout.execute(EnterAlternateScreen)?;
+    stdout.execute(Hide)?;
+
+    audio.wait();
+    stdout.execute(Show)?;
+    stdout.execute(LeaveAlternateScreen)?;
+    terminal::disable_raw_mode()?;
 
     Ok(())
 }
