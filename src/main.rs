b64cfb2baf3f8a6eae5b5f897c7d6feeb5515af1
Hendro Wibowo <hendrothemail@gmail.com>
2023-07-30 13:09:11 +0700

    create game screen on separate thread

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
 use std::error::Error;
-use std::io;
+use std::{io, thread};
+use std::sync::mpsc;
 use std::time::Duration;
 use crossterm::{event, ExecutableCommand, terminal};
 use crossterm::cursor::{Hide, Show};
 use crossterm::event::{Event, KeyCode};
 use crossterm::terminal::{EnterAlternateScreen, LeaveAlternateScreen};
 use rusty_audio::Audio;
+use invaders::{frame, render};
+use invaders::frame::new_frame;
 
 fn main() -> Result<(), Box<dyn Error>> {
     // Setup audio
@@ -18,6 +21,28 @@ fn main() -> Result<(), Box<dyn Error>> {
     audio.add("win", "./sounds/win.wav");
     audio.play("startup");
 
+    // Create a child thread for rendering.
+    // It's unnecessary but will be useful in the real world apps for a better performance.
+    let (render_tx, render_rx) = mpsc::channel();
+    let render_handle = thread::spawn(move || {
+        let mut last_frame = frame::new_frame();
+        let mut stdout = io::stdout();
+
+        render::render(&mut stdout, &last_frame, &last_frame, true);
+
+        loop {
+            let current_frame = match render_rx.recv() {
+                Ok(x) => x,
+                Err(_) => break,
+            };
+
+            render::render(&mut stdout, &last_frame, &current_frame, false);
+
+            last_frame = current_frame;
+        }
+    });
+
+
     // Setup screen
     let mut stdout = io::stdout();
     terminal::enable_raw_mode()?;
@@ -26,6 +51,8 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     // Main loop
     'mainloop: loop {
+        let current_frame = new_frame();
+
         while event::poll(Duration::default())? {
             if let Event::Key(key_event) = event::read()? {
                 match key_event.code {
@@ -37,9 +64,17 @@ fn main() -> Result<(), Box<dyn Error>> {
                 }
             }
         }
+
+        // Don't do anything if an error occurred
+        let _ = render_tx.send(current_frame);
+
+        // Delay the loop to prevent to much frame per second
+        thread::sleep(Duration::from_millis(1));
     }
 
     // Cleanup
+    drop(render_tx);
+    render_handle.join().unwrap();
     audio.wait();
     stdout.execute(Show)?;
     stdout.execute(LeaveAlternateScreen)?;
