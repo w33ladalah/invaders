e790c77c1292664cd35f1fd87550294832fc1b67
Hendro Wibowo <hendrothemail@gmail.com>
2023-07-30 14:57:07 +0700

    Create laser module

---
+++ src/laser.rs
@@ -0,0 +1,12 @@
+use rusty_time::timer::Timer;
+
+pub struct Laser {
+    pub x: usize,
+    pub y: usize,
+    pub exploded: bool,
+    timer: Timer,
+}
+
+impl Laser {
+
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 pub mod frame;
 pub mod render;
 pub mod player;
+pub mod laser;
 
 pub const NUM_ROWS: usize = 20;
 pub const NUM_COLS: usize = 40;
